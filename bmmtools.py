c50fa2444bf18164780befb3857f359de6e4e144
law909 <balint.lovey@gmail.com>
2023-09-12 22:45:28 +0200

    empty searchstring

--- bmmtools.py
+++ bmmtools.py
@@ -1,12 +1,14 @@
 import re
 
-def searchstringtofts(searchstring):    
-    keresoszo = searchstring.strip()
-    keresoszo = re.sub(r'\s+', ' ', keresoszo)
-    keresoszo = re.sub(r'([()\-])', '', keresoszo)
-    if keresoszo:
-        if not re.search(r'(["+~*])', keresoszo):
-            keresoszo = re.sub(r'([\s])', ' + ', keresoszo) + '*'
+def searchstringtofts(searchstring):
+    keresoszo = ''
+    if isinstance(searchstring, str):
+        keresoszo = searchstring.strip()
+        keresoszo = re.sub(r'\s+', ' ', keresoszo)
+        keresoszo = re.sub(r'([()\-])', '', keresoszo)
+        if keresoszo:
+            if not re.search(r'(["+~*])', keresoszo):
+                keresoszo = re.sub(r'([\s])', ' + ', keresoszo) + '*'
 
     return keresoszo
 
